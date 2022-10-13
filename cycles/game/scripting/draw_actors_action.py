1c2a2b3227b89058a9507552073f0e05ac9bf383
jlane8 <113236803+jlane8@users.noreply.github.com>
2022-10-13 07:55:35 -0500

    removed score and food

--- cycles/game/scripting/draw_actors_action.py
+++ cycles/game/scripting/draw_actors_action.py
@@ -35,9 +35,7 @@ class DrawActorsAction(Action):
         messages = cast.get_actors("messages")
 
         self._video_service.clear_buffer()
-        self._video_service.draw_actor(food)
         self._video_service.draw_actors(tread)
         self._video_service.draw_actors(tread_2)
-        self._video_service.draw_actor(score)
         self._video_service.draw_actors(messages, True)
         self._video_service.flush_buffer()
