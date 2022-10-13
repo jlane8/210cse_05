b1e6b540456ff5c191efaeca580b166baf814350
jlane8 <113236803+jlane8@users.noreply.github.com>
2022-10-13 07:59:32 -0500

    removed all trace of food

--- cycles/game/scripting/handle_collisions_action.py
+++ cycles/game/scripting/handle_collisions_action.py
@@ -36,38 +36,10 @@ class HandleCollisionsAction(Action):
             script (Script): The script of Actions in the game.
         """
         if not self._is_game_over:
-            self._handle_food_collision(cast)
             self._handle_trail_collision(cast)
             self._handle_game_over(cast)
         else:
             self._handle_game_over(cast)
-
-    # check for cycle collision with food
-    def _handle_food_collision(self, cast):
-        """Updates the food if a cycle collides with the it.
-        
-        Args:
-            cast (Cast): The cast of Actors in the game.
-        """
-        # get the food and cycle actors
-        food = cast.get_first_actor("foods")
-        cycles = cast.get_actors("cycles")
-
-        # get both cycle positions
-        head = cycles[0].get_head()
-        head_2 = cycles[1].get_head()
-
-        # if red cycle gets food, grow its trail, reset the food
-        if head.get_position().equals(food.get_position()):
-            points = food.get_points()
-            cycles[0].grow_trail(points)
-            food.reset()
-
-        # if blue cycle gets food, grow its tail, reset the food
-        if head_2.get_position().equals(food.get_position()):
-            points = food.get_points()
-            cycles[1].grow_trail(points)
-            food.reset()
     
     # method to check to see if either cycle has collided with anything
     def _handle_trail_collision(self, cast):
@@ -128,9 +100,6 @@ class HandleCollisionsAction(Action):
             trail_1 = cycles[0].get_trail()
             trail_2 = cycles[1].get_trail()
 
-            # get the food from the cast
-            food = cast.get_first_actor("foods")
-
             # set x and y coords for game over message
             x = int(constants.MAX_X / 2)
             y = int(constants.MAX_Y / 2)
@@ -160,4 +129,3 @@ class HandleCollisionsAction(Action):
                 section.set_color(constants.WHITE)
             for section in trail_2:
                 section.set_color(constants.WHITE)
-            food.set_color(constants.WHITE)
