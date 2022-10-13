51f64f76532154163dd9ed5f26f9e12e0389cc9f
jlane8 <113236803+jlane8@users.noreply.github.com>
2022-10-13 07:24:28 -0500

    updated comments

--- cycles/game/scripting/handle_collisions_action.py
+++ cycles/game/scripting/handle_collisions_action.py
@@ -1,3 +1,9 @@
+"""
+file: handle_collisions_action.py
+author: Jerry Lane and the authors of Snake
+purpose: This class exists strictly to handle collisions between actors.
+"""
+
 import constants
 from game.casting.actor import Actor
 from game.scripting.action import Action
@@ -7,18 +13,21 @@ class HandleCollisionsAction(Action):
     """
     An update action that handles interactions between the actors.
     
-    The responsibility of HandleCollisionsAction is to handle the situation when the snake collides
-    with the food, or the snake collides with its segments, or the game is over.
+    The responsibility of HandleCollisionsAction is to handle the situation when a cycle collides
+    with the food, or it collides with any trails of a cycle, or the game is over.
 
     Attributes:
         _is_game_over (boolean): Whether or not the game is over.
+        _is_winner (string): Holds the winner, or no one if it is a tie.
     """
 
+    # constructor
     def __init__(self):
         """Constructs a new HandleCollisionsAction."""
         self._is_game_over = False
         self._is_winner = ""
 
+    # the polymorphed execute method from Action
     def execute(self, cast, script):
         """Executes the handle collisions action.
 
@@ -31,37 +40,49 @@ class HandleCollisionsAction(Action):
             self._handle_trail_collision(cast)
             self._handle_game_over(cast)
 
+    # check for cycle collision with food
     def _handle_food_collision(self, cast):
-        """Updates the score nd moves the food if the snake collides with the food.
+        """Updates the food if a cycle collides with the it.
         
         Args:
             cast (Cast): The cast of Actors in the game.
         """
+        # get the food and cycle actors
         food = cast.get_first_actor("foods")
         cycles = cast.get_actors("cycles")
+
+        # get both cycle positions
         head = cycles[0].get_head()
         head_2 = cycles[1].get_head()
 
+        # if red cycle gets food, grow its trail, reset the food
         if head.get_position().equals(food.get_position()):
             points = food.get_points()
             cycles[0].grow_trail(points)
             food.reset()
 
+        # if blue cycle gets food, grow its tail, reset the food
         if head_2.get_position().equals(food.get_position()):
             points = food.get_points()
             cycles[1].grow_trail(points)
             food.reset()
     
+    # method to check to see if either cycle has collided with anything
     def _handle_trail_collision(self, cast):
         """Sets the game over flag if the snake collides with one of its segments.
         
         Args:
             cast (Cast): The cast of Actors in the game.
         """
+        # get cycle actors from the cast
         cycle = cast.get_first_actor("cycles")
         cycle_2 = cast.get_second_actor("cycles")
+        
+        # get the cycles
         head = cycle.get_trail()[0]
         head_2 = cycle_2.get_trail()[0]
+
+        # get the cycle trails
         sections_1 = cycle.get_trail()[1:]
         sections_2 = cycle_2.get_trail()[1:]
         
@@ -88,34 +109,51 @@ class HandleCollisionsAction(Action):
                 self._is_game_over = True
                 self._is_winner = "Blue"
         
+    # if game over flag is set, this method handles everything that needs to happen
     def _handle_game_over(self, cast):
         """Shows the 'game over' message and turns the snake and food white if the game is over.
         
         Args:
             cast (Cast): The cast of Actors in the game.
         """
+        # if the game over flag is set
         if self._is_game_over:
+
+            # get the cycles from the cast
             cycles = cast.get_actors("cycles")
+            
+            # get the trails from both cycles
             trail_1 = cycles[0].get_trail()
             trail_2 = cycles[1].get_trail()
+
+            # get the food from the cast
             food = cast.get_first_actor("foods")
 
+            # set x and y coords for game over message
             x = int(constants.MAX_X / 2)
             y = int(constants.MAX_Y / 2)
             position = Point(x, y)
 
+            # set up game over message to be displayed
             message = Actor()
             message.set_text("Game Over!")
             message.set_position(position)
             cast.add_actor("messages", message)
 
-
+            # set x and y coords for winner message
             position = Point(x, int(y /2))
+
+            # set up winner message for display
             winner = Actor()
+            if self._is_winner == "Red":
+                winner.set_color(constants.RED)
+            elif self._is_winner == "Blue":
+                winner.set_color(constants.BLUE)
             winner.set_text(f"{self._is_winner} Wins!")
             winner.set_position(position)
             cast.add_actor("messages", winner)
 
+            # turn everything white to signify game over
             for section in trail_1:
                 section.set_color(constants.WHITE)
             for section in trail_2:
